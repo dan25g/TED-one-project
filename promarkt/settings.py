d69a45ee3dd7c6c7481a5417125d400c9b7446d3
notaregularrick <rjmejias.17@est.ucab.edu.ve>
2023-10-31 21:26:37 -0400

    edit casi funciona

--- promarkt/settings.py
+++ promarkt/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'report',
     'base'
+    
 ]
 
 MIDDLEWARE = [
@@ -121,6 +123,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIR =[
+    BASE_DIR / "static",
+    ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
